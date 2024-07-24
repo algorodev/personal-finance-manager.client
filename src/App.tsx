1674b95f3f0a4a2e0d3556d23189b6fa874cda9a
Alejandro Gonzalez Romero <me@algorodev.com>
2024-07-24 18:46:34 +0100

    feat: client project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App = () => (
+  <div className="app">
+    <header className="header">
+      <h1>Hello, Vite + React + TypeScript!</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </header>
+  </div>
+)
+
+export default App
